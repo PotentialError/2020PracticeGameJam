af7b3220c2eafdf6f3e235bdf7f667e191cf9773
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-06-27 14:33:45 -0400

    Intro to Animation

---
+++ Assets/testingAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testingAnimation : MonoBehaviour
+{
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("f"))
+        {
+            ani.SetBool("fKeyPressed", true);
+        }
+        else
+        {
+            ani.SetBool("fKeyPressed", false);
+        }
+    }
+}
