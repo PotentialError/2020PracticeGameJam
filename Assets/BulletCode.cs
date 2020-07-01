f65f2f54ec57d05d1c6a0a7afc482d646b34f4dd
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-06-30 22:13:44 -0400

    Added SchmanWithGun

---
+++ Assets/BulletCode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCode : MonoBehaviour
+{
+    public float speed = 20;
+    public float damage = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > 100 || transform.position.x < -100)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Patrol enemy = collision.GetComponent<Patrol>();
+        if(enemy != null)
+        {
+            enemy.damage(damage);
+        }
+        if(collision.name != "SchmanWithGun")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform shootPoint;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Shooting();
+        }
+    }
+    private void Shooting()
+    {
+        Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+    }
+}
