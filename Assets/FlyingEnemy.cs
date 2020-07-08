f57eb2df61d1ea419fc241247d881c1300331e06
Michael Rodyushkin <61915029+PotentialError@users.noreply.github.com>
2020-07-08 18:45:50 -0400

    Enemy Pathfinder Complete

---
+++ Assets/FlyingEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemy : MonoBehaviour
+{
+    public float health = 3;
+    public float dazedTime;
+    private float currentTime;
+    public GameObject explosion;
+    public AudioSource hurt;
+    public AudioSource die;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            die.Play();
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+    public void damage(float damageAmount)
+    {
+        if (health > 1)
+        {
+            hurt.Play();
+        }
+        health--;
+    }
+}
